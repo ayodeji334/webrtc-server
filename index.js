5830f4ae0062c7689d5d2df11f505e56ba98c571
ayodeji334 <ojfawumi@student.lautech.edu.ng>
2022-03-07 02:18:35 -0800

    change the setup

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const cors = require('cors');
+const users = [];
+const classrooms = [];
+const usersInClassrooms = [];
 
 const io = new Server(server, {
     cors: {
@@ -21,33 +24,110 @@ app.get('/', (req, res) => {
     res.send("<h1>Welcome</h1>")
 })
 
-// start class endpoint
-app.get('/start-class', (req, res) => {
-    // console.log(req);  
-    res.send('<h1>Welcome to Start Classroom endpoint</h1>')
-});
-
 // socket connection
 io.on('connection', (socket) => {
     socket.on('join-class', (data) => {
-        console.log("this is the user Id: ", data.userID)
-        io.to(data.classID).emit('class-announcement', data);
-        // join the user to the class
-        socket.join(data.classID);
-        // broadcast the message to all conected users
-        socket.to(data.classID).emit('user-joined', { userID: data.userID, username: data.userName});
-        // join the new user to the class stream
-        // socket.to(data.classID).emit('join-class-stream', {signal: data.signalData, userID: data.userID, username: data.userName});
-        io.to(data.classID).emit('signal', {signal: data.signalData, userID: data.userID, username: data.userName});
+        const { classID, username, uid } = data;
+        const user = users.filter(user => user.uid === uid);
+        const classroom = classrooms.filter(classroom => classroom.id === classID);
+        const usersInClassroom = usersInClassrooms.filter(user => user.uid === uid && classID)
+            
+        // check if the class exist
+        if(classroom.length > 0){
+            if(user.length === 0) {
+                // save user data
+                users.push({uid, username});
+            }
+
+            if(usersInClassroom.length === 0){
+                usersInClassrooms.push({ uid, classID });
+            }
+        }else{
+            // error message
+            socket.emit('class-does-not-exist', `class with id: ${classID} does not exist`);
+        }
+
+        // const classArr = classrooms.filter((classObj) => classObj.id === data.classID);
+        // const usersInTheClassroom = users.filter(user => user.joinedClassID === data.classID);
+
+        // socket.emit('all-users', usersInTheClassroom);
     });
 
     socket.on('start-class', (data) => {
-        console.log('class id with signal data', data.classID);
+        // check if the classID and user already exist
+        const classArr = classrooms.filter((classObj) => classObj.id === data.classID);
+        const userArr = users.filter(user => user.uid === data.uid);
+        const { classTitle, classID, uid } = data;
+
+        if(classArr.length > 0){ 
+            socket.emit('class-already-created', classArr[0]);
+        }else{
+            classrooms.push({ classTitle, classID, hostID: uid});
+        }
+
+        if(userArr.length === 0){
+            users.push({...data});
+        }
+
+        socket.emit('class-created', { success: true});
+
         // broadcast a message to the class
-        io.to(data.classID).emit('class-announcement', data);
-        // sending the host signal to the class
-        io.sockets.in(data.classID).emit('signal', {signal: data.signalData, host: data.hostID, hostname: data.hostName});
+        io.to(data.classID).emit('class-started', data);
+
+        // console.log('classes: ', classrooms)
+        // console.log('users: ', users)
+
+        // // sending the host signal to the class
+        // socket.to(data.classID).emit('signal', {signal: data.signalData, host: data.hostID, hostname: data.hostName});
     });
+
+    socket.on('enter-classroom', (id) => {
+        const { classID, username, uid } = data;
+        const user = users.filter(user => user.uid === uid);
+        const classroom = classrooms.filter(classroom => classroom.id === classID);
+        const usersInTheClassroom = users.filter(user => user.joinedClassID === data.classID);
+            
+        // check if the class exist
+        if(classroom.length > 0){
+            if(user.length === 0) {
+                // save user data
+                users.push({uid, username});
+            }
+
+            socket.emit('all-users', usersInTheClassroom);
+        }else{
+            // error message
+            socket.emit('class-does-not-exist', `class with id: ${data.classID} does not exist`);
+        }
+    });
+
+    socket.on('sending-signal', (data) => {
+        io.to(data.classID).emit('joined-user-signal', data);
+    });
+
+    socket.on('returning-signal', (data) => {
+        io.to(data.uid).emit('receiving-returned-user-signal', data);
+    });
+
+    socket.on('get-current-user', (id) => {
+        const userArr = users.filter(user => user.uid === id);
+
+        if(userArr.length > 0) {
+            socket.emit('user-detail', userArr[0]);
+        }else{
+            socket.emit('user-not-found', `user account with ID: ${id} not found`);
+        }
+    });
+
+    socket.on('get-class-detail', (id) => {
+        const classroom = classrooms.filter(classroom => classroom.classID === id);
+
+        if(classroom.length > 0) {
+            socket.emit('classroom-detail', { ...classroom });
+        }else{
+            socket.emit('class-not-found', { isFound: false});
+        }
+    })
 })
 
 server.listen(9000);
